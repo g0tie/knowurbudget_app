50a20ee7770665cdef711d8ffc03f14530951e94
g0tie <gaultierbegue@outlook.com>
2022-09-20 11:41:57 +0400

    add default use choice when login screen + add responsiveness

--- src/layout/Login.jsx
+++ src/layout/Login.jsx
@@ -3,13 +3,14 @@ import { login, syncData } from "../api";
 import { useNavigate } from 'react-router-dom'
 import Alert from "../components/Alert";
 import { useMainContext } from "../store/contexts";
+import { getDefaultUserData } from '../helpers/common';
 import { getCurrentUser, getJWT, setCurrentUser, setJWT } from "../store/database";
 
 const Login = ({}) => {
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
     const navigate = useNavigate();
-    const {dispatch} = useMainContext();
+    const {state, dispatch} = useMainContext();
     const [isVisible, setVisible] = useState(false);
 
     async function handleSubmit (e) {
@@ -42,6 +43,16 @@ const Login = ({}) => {
       navigate("/");
     }
 
+    async function switchToDefaultUser()
+    {
+      await setCurrentUser(0);
+      const newState = await getDefaultUserData(state);
+      await console.log(newState)
+      await dispatch({type:"initContext", payload: newState});
+      await navigate("/");
+      await window.localStorage.removeItem("logged");
+    }
+
     return (
         
         <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
@@ -50,6 +61,7 @@ const Login = ({}) => {
       <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark.svg?color=indigo&shade=600" alt="Workflow" />
       <h2 className="mt-6 text-center text-3xl tracking-tight font-bold text-gray-900">Connexion</h2>
     </div>
+    <button onClick={switchToDefaultUser} className="underline">Utilisateur hors ligne</button>
     <Alert isVisible={isVisible}/>
     <form className="mt-8 space-y-6" action="#" method="POST">
       <input type="hidden" name="remember" value="true" />
